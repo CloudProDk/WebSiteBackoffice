d1e71af3442a2cb0c6b13790293883d5e746f4fc
lucalarsen <lucalarsen@hotmail.com>
2020-02-18 13:53:07 +0100

    slettet import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,7 @@ import {MatToolbarModule, MatButtonModule} from '@angular/material/';
 import {MatIconModule} from '@angular/material/icon';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 
