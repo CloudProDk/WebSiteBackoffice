f5f1d83e8d67ff50fe738016b5070661a807a1f7
lucalarsen <lucalarsen@hotmail.com>
2020-02-20 11:08:16 +0100

    ændret folder name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
 import { SubCategoryComponent } from './category/sub-category/sub-category.component';
+import { ReferenceComponent } from './category/sub-category/reference/reference.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/category', pathMatch: 'full' },
   { path: 'category', component: CategoryComponent},
   { path: 'sub-category', component: SubCategoryComponent},
+  { path: 'reference', component: ReferenceComponent}
 
 ];
 
