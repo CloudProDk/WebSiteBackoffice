59cbe3826a7d318280f2cadbba2ac7addaeb0f39
felex <felixmose@msn.com>
2020-03-10 14:28:32 +0100

    Update category.service.ts

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -55,7 +55,7 @@ cat4: Category = {imagePath: 'https://www.seekpng.com/png/detail/28-287453_outbo
 
   getAllCategories() {
     return this.http
-    .get<Category[]>('https://localhost:44370/api/category');
+    .get<Category[]>('http://cloudprobackofficeapi.azurewebsites.net/api/category');
   }
 
   // listOfCategory() {
